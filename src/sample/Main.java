4bf72f01a4e4b985b90d4f243bf236a23f608eae
RSK-Robin <aasrobinen6989@mail.ru>
2020-06-23 01:32:28 +0300

    fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));*/
         ViewManager manager = new ViewManager();
         primaryStage = manager.getMainStage();
         primaryStage.show();
